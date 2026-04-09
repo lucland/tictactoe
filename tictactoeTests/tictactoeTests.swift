6458d755767654118739dbe4c4d94de74ab9ca32
lucland <lucas.vsc@gmail.com>
2026-04-09 19:08:22 -0300

    Initial Commit

---
+++ tictactoeTests/tictactoeTests.swift
@@ -0,0 +1,19 @@
+//
+//  tictactoeTests.swift
+//  tictactoeTests
+//
+//  Created by Lucas Valente on 09/04/26.
+//
+
+import Testing
+@testable import tictactoe
+
+struct tictactoeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
